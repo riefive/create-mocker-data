965a39c59a22bd7556a60c79dd5fb6ebb52f68a2
riefive <muhammad.rifai@salt.co.id>
2024-01-04 13:30:54 +0700

    feat(user): add generator random user

---
+++ index.js
@@ -0,0 +1,33 @@
+const mocker = require('mocker-data-generator').default; // (vanilla way)
+const { faker } = require('@faker-js/faker');
+
+let userSchema = {
+    firstName: {
+        faker: 'person.fullName()'
+    },
+    lastName: {
+        faker: 'person.lastName()'
+    },
+    username: {
+        function: () => {
+            return faker.person.lastName().substring(0, 5) + Math.floor(Math.random() * 10);
+        }
+    },
+    email: {
+        function: () => {
+            return String(faker.internet.email()).toLowerCase();
+        }
+    },
+    country: {
+        faker: 'location.country()'
+    },
+    createdAt: {
+        faker: 'date.past()'
+    },
+};
+
+mocker()
+.addGenerator('faker', faker)
+.schema('users', userSchema, 15).build().then(data => {
+    console.log(data);
+});
